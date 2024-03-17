eaf7867c61d23ae6f15ac1c8d289f09aee16df03
NwaoghorPraise2 <chukunwaoghor@gmail.com>
2024-03-17 22:26:39 +0000

    Arr

--- src/Learning/Arras.java
+++ src/Learning/Arras.java
@@ -2,6 +2,27 @@ package Learning;
 
 public class Arras {
     public static void main(String[] args) {
+        int [] num = {3, 7, 2, 4};
+
+        num [1] = 6;
+
+        System.out.println(num[2]);
+        System.out.println(num[1]);
+
+        int [] num1  = new int[3];
+        num1[0] = 5;
+        num1[1] = 7;
+        num1[2] = 6;
+
+        for (int i = 0; i < 3; i++ ) {
+            System.out.println(num1[i]);
+        }
+
+        //Multi Array
+
+        int [] [] num2 = new int [3][4];
+
+
 
     }
 }
