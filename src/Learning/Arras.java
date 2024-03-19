179ac48fec205395eaae44167ed1b983096af498
Chukunweiken <nc23abn@herts.ac.uk>
2024-03-19 22:53:03 +0000

    Updating

--- src/Learning/Arras.java
+++ src/Learning/Arras.java
@@ -23,6 +23,48 @@ public class Arras {
         int [] [] num2 = new int [3][4];
 
 
+        for (int i =0; i < 3; i++){
+            for(int j = 0; j < 4; j++){
+                num2[i][j] = (int)(Math.random() * 10);
+            }
+        }
+
+            for (int i =0; i < 3; i++){
+                for(int j = 0; j < 4; j++){
+                    System.out.print(num2[i][j] + " ");
+                }
+                System.out.println();
+            }
+
+//            enhaned for loop
+            for (int []n : num2) {
+                for( int m : n){
+                    System.out.print(m + " ");
+                }
+                System.out.println();
+            }
+
+//            Jaggaed array
+            int [][] num3 = new int[3][];
+
+            num3[0] = new int[3];
+            num3[1] = new int[4];
+            num3[2] = new int[7];
+
+            for (int i = 0; i < num3.length; i++) {
+                for (int j = 0; j < num3[i].length; j++) {
+                   num3[i][j] = (int)(Math.random() *10);
+                }
+            }
+
+            for (int [] n : num3) {
+                for( int m : n) {
+                    System.out.print(m + " ");
+                }
+                System.out.println();
+            }
+
+
 
     }
 }
