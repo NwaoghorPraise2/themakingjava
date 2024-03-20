8fe459e817385a8e50499ac49702b8057f579b3e
Chukunweiken <nc23abn@herts.ac.uk>
2024-03-20 22:26:56 +0000

    Done

--- src/Learning/Array.java
+++ src/Learning/Array.java
@@ -1,2 +1,39 @@
-package Learning;public class Array {
+package Learning;
+class Student {
+    int rollno;
+    String name;
+    int marks;
+}
+public class Array {
+    public static void main(String[] args) {
+//        int[] numArray = {2, 4, 5, 6};
+//        numArray[3] = 7;
+//        System.out.println( numArray[3]);
+//
+//        int[] ArrayWithout = new int[10];
+        Student s1 = new Student();
+        s1.name = "James";
+        s1.marks = 10;
+        s1.rollno = 56;
+
+        Student s2 = new Student();
+        s2.name = "Praise";
+        s2.marks = 10;
+        s2.rollno = 34;
+
+        Student s3 = new Student();
+        s3.name = "Love";
+        s3.marks = 10;
+        s3.rollno = 4;
+
+        Student []students = new Student[3];
+        students[0] = s1;
+        students[1] = s2;
+        students[2] = s3;
+
+        for (Student student : students) {
+            System.out.println(STR."\{student.name} \{student.marks}");
+        }
+
+    }
 }
