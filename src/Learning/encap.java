09413ff6b2667df92af3005259d1579b39df84cf
Chukunweiken <nc23abn@herts.ac.uk>
2024-03-21 16:20:00 +0000

    progress

--- src/Learning/encap.java
+++ src/Learning/encap.java
@@ -1,5 +1,13 @@
 package Learning;
 
+class Human {
+
+    //Anytime you create an instance variale, make it private
+    private int age;
+    private String name;
+}
+
+
 public class encap {
     public static void main(String[] args) {
 
