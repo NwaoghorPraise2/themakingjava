13933bc081edae1923bae7d33ebf57eab435a59e
Chukunweiken <nc23abn@herts.ac.uk>
2024-03-21 14:47:01 +0000

    Sring Wahala

--- src/Learning/LearnString.java
+++ src/Learning/LearnString.java
@@ -3,5 +3,14 @@ package Learning;
 public class LearnString {
     public static void main(String[] args) {
         String name = "Praise";
+        System.out.println(STR."hello\{name}");
+        System.out.println(name.charAt(2));
+        System.out.println(name.concat(" Guy"));
+
+        StringBuffer sb = new StringBuffer("Praise");
+        System.out.println(sb.capacity());
+        System.out.println(sb.length());
+
+
     }
 }
