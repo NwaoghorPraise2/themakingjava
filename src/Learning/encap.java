a75cfccc412658c9b57b7547908134df2bf7269e
Chukunweiken <nc23abn@herts.ac.uk>
2024-03-21 16:59:37 +0000

    progress

--- src/Learning/encap.java
+++ src/Learning/encap.java
@@ -4,12 +4,32 @@ class Human {
 
     //Anytime you create an instance variale, make it private
     private int age;
-    private String name;
+    private  String name;
+
+    public int getAge () {
+        return age;
+    }
+
+    public void setAge (int age) {
+        this.age = age;
+    }
+
+    public String getName () {
+        return name;
+    }
+
+    public void setName( String name) {
+        this.name = name;
+    }
 }
 
 
 public class encap {
     public static void main(String[] args) {
+    Human obj = new Human();
 
+    obj.setAge(90);
+    obj.setName("Praise");
+    System.out.println(STR."\{obj.getAge()} \{obj.getName()}");
     }
 }
