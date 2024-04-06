df5af8c3789d1cca939c8df3f370351e343bfc0b
Nwaoghor Praise Chukunweiken <nwaoghorpraise@admins-air.home>
2024-04-06 21:25:41 +0100

    learn method overlading

--- src/Learning/methodOver.java
+++ src/Learning/methodOver.java
@@ -5,17 +5,25 @@ package Learning;
  */
 class A { 
     public void m1() {
-        System.out.println("Show");
+        System.out.println("In Show");
+    }
+
+    public void config(){
+        System.out.println("In Config");
     }
     
 }
 
+class B extends A {
+    public void m1() {
+        System.out.println("Show");
+    }
+}
 
 
-public class methodOver {
-    
+public class methodOver{
     public static void main(String[] args) {
-        A a = new A();
-        a.m1();
+        A a = new B();
+        System.out.println(a.m1());
     }
 }
